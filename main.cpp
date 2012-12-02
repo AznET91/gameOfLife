6ac6e8ce8299eef4c85351cce2275245edd21a67
charles long <chars.long@gmail.com>
2012-12-01 22:03:16 -0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: Charles
+ *
+ * Created on December 1, 2012, 9:54 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    //ok it should start working right?
+    
+
+    return 0;
+}
+
